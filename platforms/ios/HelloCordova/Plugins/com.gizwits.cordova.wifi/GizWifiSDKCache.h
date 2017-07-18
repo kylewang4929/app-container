6892f0b9c5f19abb367ed61ff0641f3c696b3d11
王锐彬 <492960423@qq.com>
2017-07-18 11:27:57 +0800

    精简工程

--- platforms/ios/HelloCordova/Plugins/com.gizwits.cordova.wifi/GizWifiSDKCache.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "GizWifiSDK.h"
-
-@interface GizWifiSDKCache : NSObject
-
-+ (void)addDelegate:(id <GizWifiSDKDelegate>)delegate;
-+ (void)removeDelegate:(id <GizWifiSDKDelegate>)delegate;
-
-@end
--- plugins/com.gizwits.cordova.wifi/src/ios/DeviceCache/GizWifiCacheCommon.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface GizWifiCacheCommon : NSObject
-
-+ (void)addDelegate:(id)delegate mutableArray:(NSMutableArray *)mDelegates;
-+ (void)removeDelegate:(id)delegate mutableArray:(NSMutableArray *)mDelegates;
-
-@end
